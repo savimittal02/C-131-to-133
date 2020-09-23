5b8df6692761e88e305d3ec4d8c96a19ecb61f38
savimittal02 <71250413+savimittal02@users.noreply.github.com>
2020-09-23 19:55:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 img ="";
 status ="";
+objects = [];
 
 function preload()
 {
-    img = loadImage('dog_cat.jpg');
-}
-
-function modelLoaded()
-{
-    console.log("Model Loaded!")
-    status = true;
-    objectDetector.detect(img, gotResults);
+    img = loadImage('https://i.postimg.cc/5ttZds1P/dog-cat.jpg');
 }
 
 function setup() 
@@ -21,35 +15,38 @@ function setup()
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
 function gotResult(error, results)
 {
     if (error) {
         console.log(error);
     }
-    console.log(results)
+    console.log(results);
+    objects = results;
 }
 
 function draw()
- { 
+{ 
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75); 
-    noFill();
-    stroke("yellow"); 
-    strokeWeight(2) 
-    rect(30, 60, 450, 350 ); 
 
-    fill("#FF0000");
-    text("Cat", 320, 120); 
-    noFill();
-    stroke("blue"); 
-    strokeWeight(2) 
-    rect(300, 60, 300, 350 ); 
+    if(status != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
 
-    fill("#FF0000");
-    text("Bowl", 320, 370); 
-    noFill();
-    stroke("red");  
-    strokeWeight(2) 
-    rect(280, 320, 120, 100 ); 
-}
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}  
